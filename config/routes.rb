ccd49089fae2b141c00bb3965e0caf5d2afb9fbd
ignored-comment <info.konham@gmail.com>
2021-04-23 11:10:21 -0400

    Create Remove Passenger button; assertions pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/flights', to: 'flights#index'
+  delete '/flights', to: 'flights#delete', as: 'remove_this_passenger'
 end
