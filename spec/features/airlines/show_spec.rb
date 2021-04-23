b028d4c22f76e47f85d8811a80b0bb2dbbc01035
ignored-comment <info.konham@gmail.com>
2021-04-23 12:28:42 -0400

    Create assertion for airline show page - visit airline show page; assertions fail

--- spec/features/airlines/show_spec.rb
+++ spec/features/airlines/show_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Airlines show page", type: :feature do
-  
+  before :each do
+    @murican = Airline.create(name: "Murican Airlines")
+    @murican_flight1 = @murican.flights.create(number: "1023", date: "04/01/2025", departure_city: "Mexico City", arrival_city: "New York City")
+    @flight1_passenger1 = @murican_flight1.passengers.create(name: "Bob", age: 99)
+    @flight1_passenger2 = @murican_flight1.passengers.create(name: "Bobette", age: 25)
+    @flight1_passenger3 = @murican_flight1.passengers.create(name: "Rob", age: 12)
+
+    @murican_flight2 = @murican.flights.create(number: "9000", date: "04/01/2025", departure_city: "Los Angeles", arrival_city: "San Francisco")
+    @flight2_passenger1 = @murican_flight2.passengers.create(name: "Zob", age: 45)
+    @flight2_passenger2 = @murican_flight2.passengers.create(name: "Zobette", age: 13)
+    @flight2_passenger3 = @murican_flight2.passengers.create(name: "Pob", age: 17)
+  end
+
+  describe "when I visit an airline's show page" do
+    it 'shows me a list of passengers that have flights on that airline' do
+      visit "/airline/#{@murican.id}"
+
+      
+    end
+  end
 end
