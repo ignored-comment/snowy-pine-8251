6e1519c213a53ff2d7bd21e396ac6876e01eea82
ignored-comment <info.konham@gmail.com>
2021-04-23 10:39:27 -0400

    Create assertions for flight numbers; assertions failing

--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'Flights Index Page', type: :feature do
     @flight1_passenger2 = @murican_flight1.passengers.create(name: "Bobette", age: 25)
     @flight1_passenger3 = @murican_flight1.passengers.create(name: "Rob", age: 12)
 
-    @murican_flight2 = @murican.flights.create(number: "1023", date: "04/01/2025", departure_city: "Los Angeles", arrival_city: "San Francisco")
+    @murican_flight2 = @murican.flights.create(number: "9000", date: "04/01/2025", departure_city: "Los Angeles", arrival_city: "San Francisco")
     @flight2_passenger1 = @murican_flight2.passengers.create(name: "Zob", age: 45)
     @flight2_passenger2 = @murican_flight2.passengers.create(name: "Zobette", age: 13)
     @flight2_passenger3 = @murican_flight2.passengers.create(name: "Pob", age: 17)
@@ -16,6 +16,10 @@ RSpec.describe 'Flights Index Page', type: :feature do
 
   describe 'when I visit the flights index page' do
     it 'can show me a list of all flight numbers' do
+      visit '/flights'
+
+      expect(page).to have_content("1023")
+      expect(page).to have_content("9000")
     end
 
     it 'and next to each flight number I can see the name of the airline of that flight' do
